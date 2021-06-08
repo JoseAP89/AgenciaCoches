0c690a77d4b19f9f13992475a7dcad802d4440cc
Salvador <angelcantu086@gmail.com>
2021-06-08 13:27:04 -0500

    Interfaz del modelo automovil

---
+++ src/Models/IAutomovil.java
@@ -0,0 +1,14 @@
+/*
+ * Interfaz para el modelo Automovil
+ * 
+ * 
+ */
+package Models;
+
+/**
+ *
+ * @author Salvador Solis Atenco
+ */
+public interface IAutomovil {
+    public void ordenarAuto(Automovil auto);
+}
