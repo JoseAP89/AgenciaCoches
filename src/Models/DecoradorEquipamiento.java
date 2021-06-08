a780c8ef6efcf04c4e9935fcf90b3a7088dde472
Salvador <angelcantu086@gmail.com>
2021-06-08 13:26:46 -0500

    Clase abstracta para el decorador

---
+++ src/Models/DecoradorEquipamiento.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author Salvador Solis Atenco
+ */
+public abstract class DecoradorEquipamiento implements IAutomovil {
+    IAutomovil AutoDecorado;
+
+    public DecoradorEquipamiento(IAutomovil AutoDecorado) {
+        this.AutoDecorado = AutoDecorado;
+    }
+    
+    @Override
+    public void ordenarAuto(Automovil auto){
+        this.AutoDecorado.ordenarAuto(auto);
+    }
+    
+}
