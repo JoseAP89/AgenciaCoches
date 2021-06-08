5b50f14daf94b11e6f02872e66508d28f4d19af5
Salvador <angelcantu086@gmail.com>
2021-06-07 20:30:35 -0500

    Create Carro.java
    Clase modelo Carro

---
+++ src/Controllers/Carro.java
@@ -0,0 +1,28 @@
+package Controllers;
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private String rendimiento;
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public String getRendimiento() {
+        return rendimiento;
+    }
+    public void setRendimiento(String rendimiento) {
+        this.rendimiento = rendimiento;
+    }
+
+    
+
+}
